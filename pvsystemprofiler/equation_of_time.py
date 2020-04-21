583bee2e949847bd1a62a788091d29cf5b0438ec
Bennet Meyers <bennetm@stanford.edu>
2020-04-21 15:36:03 -0700

    Cleaned up EoT calculation in new module and double-checked against source materials

--- pvsystemprofiler/equation_of_time.py
+++ pvsystemprofiler/equation_of_time.py
@@ -8,7 +8,33 @@ import numpy as np
 
 
 def haghdadi(day_of_year):
-    pass
+    """
+    The equation of time as defined in:
+        Haghdadi, Navid, et al. "A method to estimate the location and
+        orientation of distributed photovoltaic systems from their generation
+        output data." Renewable Energy 108 (2017): 390-400.
+
+    These are equations (7) and (8) in the paper.
+
+    :param day_of_year: the day of year, can be int, float, or numpy array
+    :return: the difference between clock time and solar time for a given day of year
+    """
+    b = np.deg2rad((360 / 365) * (day_of_year - 81))
+    eot = 9.87 * np.sin(2 * b) - 7.53 * np.cos(b) - 1.5 * np.sin(b)
+    return eot
 
 def duffie(day_of_year):
-    pass
+    """
+    The equation of time as defined in:
+        Duffie, John A., and William A. Beckman. Solar engineering of thermal
+        processes. New York: Wiley, 1991.
+
+    These are equations (1.4.2) and (1.5.3) in the book
+
+    :param day_of_year: the day of year, can be int, float, or numpy array
+    :return: the difference between clock time and solar time for a given day of year
+    """
+    b = np.deg2rad((360 / 365) * day_of_year - 1)
+    eot = 229.2 * (0.000075 + 0.001868 * np.cos(b) - 0.032077 * np.sin(b)
+                   - 0.014615 * np.cos(2 * b) - 0.04089 * np.sin(2 * b))
+    return eot
