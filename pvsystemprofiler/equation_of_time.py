9f98d864dc2fa51d7b353669a8d000402a6a9a99
Bennet Meyers <bennetm@stanford.edu>
2020-04-21 15:24:26 -0700

    structure for new equation of time module

---
+++ pvsystemprofiler/equation_of_time.py
@@ -0,0 +1,14 @@
+''' Equation of Time Module
+The Equation of Time (EoT) describes the discrepancy between clock time and
+solar time: https://en.wikipedia.org/wiki/Equation_of_time
+
+This module contains two approaches to calculating the EoT: Haghdadi and Duffie
+'''
+import numpy as np
+
+
+def haghdadi(day_of_year):
+    pass
+
+def duffie(day_of_year):
+    pass
