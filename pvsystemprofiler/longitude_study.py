cf56b01a243bcae327e4f75c22da05ea260bd311
Bennet Meyers <bennetm@stanford.edu>
2020-04-21 21:29:25 -0700

    expanded study procedure

--- pvsystemprofiler/longitude_study.py
+++ pvsystemprofiler/longitude_study.py
@@ -15,6 +15,7 @@ import cvxpy as cvx
 from solardatatools.solar_noon import energy_com, avg_sunrise_sunset
 from pvsystemprofiler.algorithms.longitude.direct_calculation import calc_lon
 from pvsystemprofiler.utilities.equation_of_time import eot_haghdadi, eot_duffie
+from pvsystemprofiler.utilities.progress import progress
 
 class LongitudeStudy():
     def __init__(self, data_handler, day_selection="cloudy days",
@@ -45,28 +46,64 @@ class LongitudeStudy():
         else:
             self.days = np.ones(self.data_matrix.shape[1], dtype=np.bool)
 
-    def run(self):
-        results = pd.DataFrame(columns=['calculated', 'fit_l1', 'fit_l2',
-                                        'fit_huber'])
-        keys = ['duffie', 'haghdadi']
-        for key in keys:
-            results.loc[key] = [
-                self.calculate_longitude(eot_ref=key),
-                self.fit_longitude(loss='l1', eot_ref=key),
-                self.fit_longitude(loss='l2', eot_ref=key),
-                self.fit_longitude(loss='huber', eot_ref=key),
-            ]
+    def run(self, estimator=('calculated', 'fit_l1', 'fit_l2', 'fit_huber'),
+            eot_calculation=('duffie', 'haghdadi'),
+            solar_noon_method=('rise_set_average', 'energy_com'),
+            day_selection_method=('all', 'clear', 'cloudy')):
+        results = pd.DataFrame(columns=[
+            'longitude', 'estimator', 'eot_calculation', 'solar_noon_method',
+            'day_selection_method'
+        ])
+        estimator = np.atleast_1d(estimator)
+        eot_calculation = np.atleast_1d(eot_calculation)
+        solar_noon_method = np.atleast_1d(solar_noon_method)
+        day_selection_method = np.atleast_1d(day_selection_method)
+        total = (len(estimator) * len(eot_calculation) * len(solar_noon_method)
+                 * len(day_selection_method))
+        counter = 0
+        for est in estimator:
+            for eot in eot_calculation:
+                pass
+        for sn in solar_noon_method:
+            if sn == 'rise_set_average':
+                self.solarnoon = avg_sunrise_sunset(self.data_matrix)
+            elif sn == 'energy_com':
+                self.solarnoon = energy_com(self.data_matrix)
+            for ds in day_selection_method:
+                if ds == 'all':
+                    self.days = np.ones(self.data_matrix.shape[1],
+                                        dtype=np.bool)
+                elif ds == 'clear':
+                    self.days = self.data_handler.daily_flags.clear
+                elif ds == 'cloudy':
+                    self.days = self.data_handler.daily_flags.cloudy
+                for est in estimator:
+                    for eot in eot_calculation:
+                        progress(counter, total)
+                        lon = self.estimate_longitude(est, eot)
+                        results.loc[counter] = [
+                            lon, est, eot, sn, ds
+                        ]
+                        counter += 1
+        progress(counter, total)
         return results
+
+    def estimate_longitude(self, estimator, eot_calculation):
+        if estimator == 'calculated':
+            return self.calculate_longitude(eot_ref=eot_calculation)
+        else:
+            loss = estimator.split('_')[-1]
+            return self.fit_longitude(loss=loss, eot_ref=eot_calculation)
 
     def calculate_longitude(self, eot_ref='duffie'):
         sn = 60 * self.solarnoon[self.days]  # convert hours to minutes
         if eot_ref in ('duffie', 'd', 'duf'):
-            eot = self.eot_duffie
+            eot = self.eot_duffie[self.days]
         elif eot_ref in ('haghdadi', 'h', 'hag'):
-            eot = self.eot_hag
+            eot = self.eot_hag[self.days]
         gmt = self.gmt_offset
         estimates = calc_lon(sn, eot, gmt)
-        return np.median(estimates)
+        return np.nanmedian(estimates)
 
 
     def fit_longitude(self, loss='l2', eot_ref='duffie'):
@@ -83,7 +120,9 @@ class LongitudeStudy():
             eot = self.eot_hag
         sn_m = 720 - eot + 4 * (15 * self.gmt_offset - lon)
         sn_h = sn_m / 60
-        cost = cost_func(sn_h[self.days] - self.solarnoon[self.days])
+        nan_mask = np.isnan(self.solarnoon)
+        use_days = np.logical_and(self.days, ~nan_mask)
+        cost = cost_func(sn_h[use_days] - self.solarnoon[use_days])
         objective = cvx.Minimize(cost)
         problem = cvx.Problem(objective)
         problem.solve()
