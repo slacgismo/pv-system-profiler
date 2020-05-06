da8cbac7cd16b2e87deb652b3035b81835a8c6df
alh <a@alhs-MacBook-Air.local>
2020-05-06 15:53:08 -0700

    Added run_curve_fit_1 and estimate_costheta functions Added estimated latitute as input parameter

--- pvsystemprofiler/tilt_azimuth_study.py
+++ pvsystemprofiler/tilt_azimuth_study.py
@@ -4,6 +4,7 @@
 import numpy as np
 import pandas as pd
 import cvxpy as cvx
+from scipy.optimize import curve_fit
 from solardatatools.solar_noon import energy_com, avg_sunrise_sunset
 from pvsystemprofiler.algorithms.longitude.direct_calculation import calc_lon
 from pvsystemprofiler.utilities.equation_of_time import eot_haghdadi, eot_duffie
@@ -11,7 +12,7 @@ from pvsystemprofiler.utilities.progress import progress
 from solardatatools.daytime import find_daytime
 class TiltAzimuthStudy():
     def __init__(self, data_handler, gmt_offset=-8, summer_flag=False,
-                 init_values=None, daytime_threshold=None, lat_true_value=None, tilt_true_value=None,
+                 init_values=None, daytime_threshold=None, lat_estimate=None, lat_true_value=None, tilt_true_value=None,
                  azim_true_value=None):
         self.data_handler = data_handler
         if not data_handler._ran_pipeline:
@@ -22,6 +23,7 @@ class TiltAzimuthStudy():
         self.summer_flag = summer_flag
         self.init_values = init_values
         self.daytime_threshold = daytime_threshold
+        self.latitude_estimate = lat_estimate
         self.phi_real = lat_true_value
         self.ground_beta = tilt_true_value
         self.ground_gamma = azim_true_value
@@ -54,21 +56,17 @@ class TiltAzimuthStudy():
             self.slct_summer = np.ones(self.day_of_year.shape, dtype=bool)
 
         self._calculate = None
-        self.latitude_estimate = None
-        self.latitude_calculate = None
-        self.latitude_estimate = None
         self.hours_daylight = None
         self.delta = None
         self.omega = None
         self.costheta_fit = None
-        self.latitude_estimate = None
         self.latitude_calculate = None
         self.tilt_estimate = None
         self.azimuth_estimate = None
         self.costheta_ground_truth_calculate = None
         self.scale_factor_costheta = None
         self.hours_daylight = None
-        self.costheta_ = None
+        self.costheta_estimated = None
         self.costheta_mean_absolute_error = None
         self.costheta_mean_squared_error = None
         self.latitude_error = None
@@ -87,6 +85,9 @@ class TiltAzimuthStudy():
            for d in range(0, self.num_days - 1):
                self.boolean_daytime[:, d] = self.data_matrix[:, d] > 1 * self.power_threshold_fit[d]
         self.select_days()
+        self.run_curve_fit_1()
+        self.estimate_costheta()
+
         return
 
     def make_delta(self):
@@ -128,6 +129,14 @@ class TiltAzimuthStudy():
             self.func2(X, phi_real_2d, ground_beta_2d, ground_gamma_2d)
         return
 
+    def estimate_costheta(self):
+        X = np.array([self.omega, self.delta])
+        phi_estimate_2d = np.tile(np.deg2rad(self.latitude_estimate),(self.daily_meas,self.num_days))
+        beta_estimate_2d = np.tile(np.deg2rad(self.tilt_estimate),(self.daily_meas,self.num_days))
+        gamma_estimate_2d = np.tile(np.deg2rad(self.azimuth_estimate),(self.daily_meas,self.num_days))
+        self.costheta_estimated = self.func2(X,phi_estimate_2d, beta_estimate_2d, gamma_estimate_2d)
+        return
+
     def func2(self, X, phi, beta, gamma):
         w = X[0]
         d = X[1]
@@ -164,3 +173,22 @@ class TiltAzimuthStudy():
             self.slct_curve_fit = self.boolean_daytime * self.clear_index_set * self.slct_summer
             self.delta_f = self.delta[self.slct_curve_fit]
             self.omega_f = self.omega[self.slct_curve_fit]
+
+
+    def run_curve_fit_1(self, bootstrap_iterations=None):
+        self.costheta_fit_f = self.costheta_fit[self.slct_curve_fit]
+        X = np.array([self.omega_f, self.delta_f])
+        popt, pcov = curve_fit(self.func, X, self.costheta_fit_f, p0=np.deg2rad(self.init_values), bounds=([0,-3.14],[1.57,3.14]))
+        self.tilt_estimate, self.azimuth_estimate = np.degrees(popt)
+        return
+
+    def func(self, X, beta, gamma):
+        w = X[0]
+        d = X[1]
+        phi = np.deg2rad(self.latitude_estimate)
+        A = np.sin(d) * np.sin(phi) * np.cos(beta)
+        B = np.sin(d) * np.cos(phi) * np.sin(beta) * np.cos(gamma)
+        C = np.cos(d) * np.cos(phi) * np.cos(beta) * np.cos(w)
+        D = np.cos(d) * np.sin(phi) * np.sin(beta) * np.cos(gamma) * np.cos(w)
+        E = np.cos(d) * np.sin(beta) * np.sin(gamma) * np.sin(w)
+        return A - B + C + D + E
