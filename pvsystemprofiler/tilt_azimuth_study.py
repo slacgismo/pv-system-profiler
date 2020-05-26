b93e34e6a56386ec6751a107e476e07c742655f6
alh <a@alhs-MacBook-Air.local>
2020-05-26 14:11:10 -0700

    added Try and Except to account for a day range is not entered

--- pvsystemprofiler/tilt_azimuth_study.py
+++ pvsystemprofiler/tilt_azimuth_study.py
@@ -14,22 +14,22 @@ import cvxpy as cvx
 from scipy.optimize import curve_fit
 from solardatatools.daytime import find_daytime
 class TiltAzimuthStudy():
-    def __init__(self, data_handler, set_day_range=None, day_range=None, init_values=None,
+    def __init__(self, data_handler, set_day_range=None, day_range=None, init_values=[10, 10],
                  daytime_threshold=None, lat_estimate=None,
                  lat_true_value=None, tilt_true_value=None,
                  azimuth_true_value=None):
         '''
-
         :param data_handler: `DataHandler` class instance loaded with a solar power data set
         :param set_day_range: (optional) True if running the study over a day range
         :param day_range: (optional) the desired day range to run the study. An array of the form
                               [first day, last day]
-        :param init_values: (optional) Initial values for numerical fit
+        :param init_values: (optional) Initial values for numerical fit. Default values of [10, 10]
+                                are used otherwise
         :param daytime_threshold: daytime threshold
         :param lat_estimate: latitude estimate as obtained from the Latitude Study module
-        :param lat_true_value: the ground truth value for the system's latitude
-        :param real_tilt: (optional) the ground truth value for the system's tilt
-        :param real_azimuth: (optional) the ground truth value for the system's azimuth
+        :param lat_true_value: (optional) the ground truth value for the system's latitude
+        :param tilt_true_value: (optional) the ground truth value for the system's tilt
+        :param azimuth_true_value: (optional) the ground truth value for the system's azimuth
         '''
 
         self.data_handler = data_handler
@@ -43,9 +43,15 @@ class TiltAzimuthStudy():
         self.daytime_threshold = daytime_threshold
         self.daytime_threshold_fit = None
         self.latitude_estimate = lat_estimate
-        self.phi_true_value = np.deg2rad(lat_true_value)
-        self.beta_true_value = np.deg2rad(tilt_true_value)
-        self.gamma_true_value = np.deg2rad(azimuth_true_value)
+        self.phi_true_value = None
+        self.beta_true_value = None
+        self.gamma_true_value = None
+        if lat_true_value is not None:
+            self.phi_true_value = np.deg2rad(lat_true_value)
+        if tilt_true_value is not None:
+            self.beta_true_value = np.deg2rad(tilt_true_value)
+        if azimuth_true_value is not None:
+            self.gamma_true_value = np.deg2rad(azimuth_true_value)
         self.day_of_year = self.data_handler.day_index.dayofyear
         self.num_days = self.data_handler.num_days
         self.daily_meas = self.data_handler.filled_data_matrix.shape[0]
@@ -67,8 +73,12 @@ class TiltAzimuthStudy():
         self.delta_f = None
         self.results = None
         if self.set_day_range:
-            self.day_range = (self.day_of_year > self.day_range[0]) & \
-                             (self.day_of_year < self.day_range[1])
+            try:
+                self.day_range = (self.day_of_year > self.day_range[0]) & \
+                                 (self.day_of_year < self.day_range[1])
+            except TypeError:
+                print('select_day_range flag is set to True but no day range was provided.\n'
+                      'Please provide day range or set select_day_range flag=False')
         else:
             self.day_range = np.ones(self.day_of_year.shape, dtype=bool)
 
@@ -77,15 +87,21 @@ class TiltAzimuthStudy():
         self.make_delta()
         self.make_omega()
         self.find_fit_costheta()
-        self.ground_truth_costheta()
+
         self.select_days()
         self.run_curve_fit_1()
         self.estimate_costheta()
-        self.results = pd.DataFrame(columns=['Latitude Residual', 'Tilt Residual', 'Azimuth Residual'])
-        r1 = np.rad2deg(self.phi_true_value) - self.latitude_estimate
-        r2 = np.rad2deg(self.beta_true_value) - self.tilt_estimate
-        r3 = np.rad2deg(self.gamma_true_value) - self.azimuth_estimate
-        self.results.loc[0] = [r1, r2, r3]
+        if self.phi_true_value is not None:
+            if self.beta_true_value is not None:
+                if self.gamma_true_value is not None:
+                    self.ground_truth_costheta()
+                    self.results = pd.DataFrame(columns=['Latitude Residual',
+                                                         'Tilt Residual',
+                                                         'Azimuth Residual'])
+                    r1 = np.rad2deg(self.phi_true_value) - self.latitude_estimate
+                    r2 = np.rad2deg(self.beta_true_value) - self.tilt_estimate
+                    r3 = np.rad2deg(self.gamma_true_value) - self.azimuth_estimate
+                    self.results.loc[0] = [r1, r2, r3]
         return
 
     def find_boolean_daytime(self):
