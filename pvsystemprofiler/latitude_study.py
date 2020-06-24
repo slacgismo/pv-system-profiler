63a01408539dd1576dc837ef851c4268ac494618
alh <a@alhs-air.lan>
2020-06-24 09:06:51 -0700

    added second equation to calculate delta

--- pvsystemprofiler/latitude_study.py
+++ pvsystemprofiler/latitude_study.py
@@ -34,19 +34,21 @@ class LatitudeStudy():
         self.boolean_daytime = None
         self.discrete_latitude = None
         self.hours_daylight = None
-        self.delta = None
+        self.delta_cooper = None
+        self.delta_spencer = None
         self.residual = None
         # Results
         self.results = None
 
     def run(self, threshold_method=('raw data matrix', 'filled data matrix'),
             daylight_method=('raw daylight', 'sunrise-sunset'),
+            delta_method=('cooper', 'spencer'),
             threshold=None):
         '''
-
         :param threshold_method: 'raw data matrix', 'filled data matrix'
         :param daylight_method: 'raw daylight', 'sunrise-sunset'
         :param threshold: (optional) daylight threshold values, tuple of length one to four
+        :param delta_method: (optional) 'cooper', 'spencer'
         :return:
         '''
         threshold_method = np.atleast_1d(threshold_method)
@@ -55,17 +57,20 @@ class LatitudeStudy():
         thres = threshold
         if threshold is None:
             thres = 0.001*np.ones(len(threshold_method)*len(daylight_method))
-        self.make_delta()
+        self.make_delta_cooper()
+        self.make_delta_spencer()
 
         results = pd.DataFrame(columns=['latitude', 'threshold', 'threshold matrix', 'daylight calculation'])
         counter = 0
-        for matrix_ix, matrix_id in enumerate(threshold_method):
-            for daylight_method_id in daylight_method:
-                dtt = thres[counter]
-                dlm = daylight_method_id
-                met = threshold_method[matrix_ix]
-                dcc = daylight_method_id
-                lat_est = self.estimate_latitude(matrix_id, daytime_threshold=dtt, daylight_method=dlm)
+        for delta_id in delta_method:
+            for matrix_ix, matrix_id in enumerate(threshold_method):
+                for daylight_method_id in daylight_method:
+                    dtt = thres[counter]
+                    dlm = daylight_method_id
+                    met = threshold_method[matrix_ix]
+                    dcc = daylight_method_id
+                    lat_est = self.estimate_latitude(matrix_id, daytime_threshold=dtt, daylight_method=dlm,
+                                                     delta_method=delta_id)
 
                 results.loc[counter] = [lat_est, dtt, met, dcc]
                 counter += 1
@@ -74,8 +79,9 @@ class LatitudeStudy():
 
         self.results = results
 
-    def estimate_latitude(self, matrix_id=None, daytime_threshold=0.001, daylight_method=('sunrise-sunset',
-                                                                                          'raw daylight')):
+    def estimate_latitude(self, matrix_id=None, daytime_threshold=0.001,
+                          daylight_method=('sunrise-sunset', 'raw daylight'),
+                          delta_method=('cooper', 'spencer')):
         """"
         Latitude is estimated from equation (1.6.11) in:
         Duffie, John A., and William A. Beckman. Solar engineering of thermal
@@ -91,14 +97,36 @@ class LatitudeStudy():
             self.hours_daylight = self.calculate_hours_daylight(data_in, daytime_threshold)
         if daylight_method in ('raw_daylight', 'raw daylight'):
             self.hours_daylight = self.calculate_hours_daylight_raw(data_in, daytime_threshold)
+        if delta_method in ('Cooper', 'cooper'):
+            delta = self.delta_cooper
+        if delta_method in ('Spencer', 'spencer'):
+            delta = self.delta_spencer
 
         self.discrete_latitude = np.degrees(np.arctan(- np.cos(np.radians(15 / 2 * self.hours_daylight)) /
-                                                      (np.tan(self.delta[0]))))
+                                                      (np.tan(delta[0]))))
         return np.median(self.discrete_latitude)
 
-    def make_delta(self):
+    def make_delta_cooper(self):
+        """"
+        Declination delta1 is estimated from equation (1.6.1a) in:
+        Duffie, John A., and William A. Beckman. Solar engineering of thermal
+        processes. New York: Wiley, 1991.
+        """
         delta_1 = np.deg2rad(23.45 * np.sin(np.deg2rad(360 * (284 + self.day_of_year) / 365)))
-        self.delta = np.tile(delta_1, (self.data_matrix.shape[0], 1))
+        self.delta_cooper = np.tile(delta_1, (self.daily_meas, 1))
+        return
+
+    def make_delta_spencer(self):
+        """"
+        Declination delta1 is estimated from equation (1.6.1b) in:
+        Duffie, John A., and William A. Beckman. Solar engineering of thermal
+        processes. New York: Wiley, 1991.
+        """
+        b = (self.day_of_year - 1)*360/365
+        delta_1 = np.deg2rad((180 / np.pi) * (0.006918 - 0.399912 * np.cos(b) + 0.070257 * np.sin(b) - 0.006758 *
+                                             np.cos(2 * b) + 0.000907 * np.sin(2 * b) - 0.002697 * np.cos(3 * b) +
+                                             0.00148 * np.sin(3 * b)))
+        self.delta_spencer = np.tile(delta_1, (self.daily_meas, 1))
         return
 
     def calculate_hours_daylight_raw(self, data_in, threshold=0.001):
