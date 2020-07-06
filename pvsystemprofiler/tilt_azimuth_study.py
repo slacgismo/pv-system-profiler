corrected omega equation to make calculation in solar time

pvsystemprofiler/tilt_azimuth_study.py
@@ -81,7 +81,9 @@ class TiltAzimuthStudy():
 
         delta_method = np.atleast_1d(delta_method)
         self.find_boolean_daytime()
-        self.omega = find_omega(self.data_sampling, self.num_days)
+        self.omega = find_omega(self.data_sampling, self.num_days, self.lon_estimate, self.day_of_year,
+                                self.gmt_offset)
+
         self.scale_factor_costheta, self.costheta_fit = self.find_fit_costheta(self.data_matrix, self.clear_index)
         self.delta_cooper = delta_cooper(self.day_of_year, self.daily_meas)
         self.delta_spencer = delta_spencer(self.day_of_year, self.daily_meas)

pvsystemprofiler/utilities/hour_angle_equation.py
@@ -1,10 +1,13 @@
 import numpy as np
+from pvsystemprofiler.utilities.time_convert import clock_to_solar
 
-def find_omega(data_sampling, num_days):
+def find_omega(data_sampling,num_days, lon, doy, gmt_offset):
         """Omega, the hour angle is estimated as defined on p. 13 in:
         Duffie, John A., and William A. Beckman. Solar engineering of thermal
         processes. New York: Wiley, 1991."""
-        hour = np.arange(0, 24, data_sampling / 60)
-        omega_1 = np.deg2rad(15 * (hour - 12))
-        omega = np.tile(omega_1.reshape(-1, 1), (1, num_days))
+        hours_day = np.arange(0, 1440, data_sampling)
+        hours_doy = np.tile(hours_day.reshape(-1, 1), (1, num_days))
+        hours_doy_solar = clock_to_solar(hours_doy, lon, doy, gmt_offset, eot='duffie')
+        hours_doy_solar = hours_doy_solar / 60
+        omega = np.deg2rad(15 * (hours_doy_solar - 12))
         return omega