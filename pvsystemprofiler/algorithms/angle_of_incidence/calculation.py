9a1ccae1610ad916cd2597b9d83a323d5b03260f
alh <a@alhs-air.lan>
2020-07-08 12:29:21 -0700

    transformed curve_fit bounds into input parameter.

--- pvsystemprofiler/algorithms/angle_of_incidence/calculation.py
+++ pvsystemprofiler/algorithms/angle_of_incidence/calculation.py
@@ -3,11 +3,11 @@ import numpy as np
 import cvxpy as cvx
 
 
-def run_curve_fit(func, delta, omega, costheta, boolean_daytime_range, init_values):
+def run_curve_fit(func, delta, omega, costheta, boolean_daytime_range, init_values, fit_bounds):
     costheta_fit = costheta[boolean_daytime_range]
     x = np.array([delta, omega])
     popt, pcov = curve_fit(func, x, costheta_fit, p0=np.deg2rad(init_values),
-                           bounds=([0, -3.14], [1.57, 3.14]))
+                           bounds=fit_bounds)
     tilt_estimate, azimuth_estimate = np.degrees(popt)
     return tilt_estimate, azimuth_estimate
 
