ff70a20f9da3c79be7c971329e5e4ecaead83ff5
alh <a@alhs-air.lan>
2020-07-08 08:35:12 -0700

    created precalculated lon, lat, tilt and azim input variables to set lambda function

--- pvsystemprofiler/tilt_azimuth_study.py
+++ pvsystemprofiler/tilt_azimuth_study.py
@@ -21,8 +21,9 @@ from pvsystemprofiler.algorithms.angle_of_incidence.calculation import calculate
 from pvsystemprofiler.utilities.angle_of_incidence_function import func_costheta
 
 class TiltAzimuthStudy():
-    def __init__(self, data_handler, day_range=None, init_values=None, daytime_threshold=None, lon_estimate=None,
-                 lat_estimate=None, lat_true_value=None, tilt_true_value=None, azimuth_true_value=None, gmt_offset=-8):
+    def __init__(self, data_handler, day_range=None, init_values=None, daytime_threshold=None, lon_precalculate=None,
+                 lat_precalculate=None, tilt_precalculate= None, azimuth_precalculate=None, lat_true_value=None,
+                 tilt_true_value=None, azimuth_true_value=None, gmt_offset=-8):
         """
         :param data_handler: `DataHandler` class instance loaded with a solar power data set
         :param day_range: (optional) the desired day range to run the study. An array of the form
@@ -31,11 +32,13 @@ class TiltAzimuthStudy():
                                 Default values are [10, 10]. (Degrees).
                                 are used otherwise
         :param daytime_threshold: (optional) daytime threshold
-        :param lat_estimate: latitude estimate as obtained from the Latitude Study module. (Degrees).
-        :param lon_estimate: latitude estimate as obtained from the Latitude Study module. (Degrees).
+        :param lon_precalculate: longitude estimate as obtained from the Longitude Study module. (Degrees).
+        :param lat_precalculate: precalculated latitude value. (Degrees).
+        :param tilt_precalculate: precalculated tilt value. (Degrees).
+        :param azimuth_precalculate: precalculated azimuth value. (Degrees).
         :param lat_true_value: (optional) ground truth value for the system's Latitude. (Degrees).
         :param tilt_true_value: (optional) ground truth value for the system's Tilt. (Degrees).
-        :param azimuth_true_value: (optional) ground truth value for the system's Azimuth. (Degrees)
+        :param azimuth_true_value: (optional) ground truth value for the system's Azimuth. (Degrees).
         :param gmt_offset: The offset in hours between the local timezone and GMT/UTC
         """
 
@@ -55,13 +58,15 @@ class TiltAzimuthStudy():
         else:
             self.init_values = init_values
         self.daytime_threshold = daytime_threshold
-        self.lon_estimate = lon_estimate
-        self.daytime_threshold_fit = None
-        self.latitude_estimate = lat_estimate
+        self.lon_precalc = lon_precalculate
+        self.lat_precalc = lat_precalculate
+        self.tilt_precalc = tilt_precalculate
+        self.azim_precalc = azimuth_precalculate
         self.phi_true_value = lat_true_value
         self.beta_true_value = tilt_true_value
         self.gamma_true_value = azimuth_true_value
         self.gmt_offset = gmt_offset
+        self.daytime_threshold_fit = None
         self.day_of_year = self.data_handler.day_index.dayofyear
         self.num_days = self.data_handler.num_days
         self.daily_meas = self.data_handler.filled_data_matrix.shape[0]
@@ -82,7 +87,7 @@ class TiltAzimuthStudy():
 
         delta_method = np.atleast_1d(delta_method)
         self.find_boolean_daytime()
-        self.omega = find_omega(self.data_sampling, self.num_days, self.lon_estimate, self.day_of_year,
+        self.omega = find_omega(self.data_sampling, self.num_days, self.lon_precalc, self.day_of_year,
                                 self.gmt_offset)
 
         self.scale_factor_costheta, self.costheta_fit = find_fit_costheta(self.data_matrix, self.clear_index)
@@ -109,16 +114,17 @@ class TiltAzimuthStudy():
                 self.omega_f = omega_f
                 self.delta = delta
 
-                func_customized = lambda x, beta, gamma: func_costheta(x, np.deg2rad(self.latitude_estimate), beta, gamma)
+                func_customized = lambda x, beta, gamma: func_costheta(x, np.deg2rad(self.lat_precalc), beta,
+                                                                       gamma)
 
                 tilt_estimate, azimuth_estimate = run_curve_fit(func=func_customized, delta=delta_f, omega=omega_f,
                                                                 costheta=self.costheta_fit,
                                                                 boolean_daytime_range=self.boolean_daytime_range,
                                                                 init_values=self.init_values)
 
-                self.costheta_estimated = calculate_costheta(func=func_costheta, delta_sys=delta, omega_sys=self.omega,
-                                                             latitude_sys=self.latitude_estimate,
-                                                             tilt_sys=tilt_estimate, azimuth_sys=azimuth_estimate)
+                # self.costheta_estimated = calculate_costheta(func=func_costheta, delta_sys=delta, omega_sys=self.omega,
+                #                                               latitude_sys=self.lat_precalc,
+                #                                               tilt_sys=tilt_estimate, azimuth_sys=azimuth_estimate)
 
                 if self.phi_true_value is not None:
                     if self.beta_true_value is not None:
