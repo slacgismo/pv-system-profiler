992ec72f6685ee99363dbe2399065c4ee414c48e
alh <a@alhs-MacBook-Air.local>
2020-07-10 09:32:09 -0700

    condensed bound selection algorithm.

--- pvsystemprofiler/algorithms/angle_of_incidence/lambda_functions.py
+++ pvsystemprofiler/algorithms/angle_of_incidence/lambda_functions.py
@@ -3,48 +3,42 @@ import numpy as np
 
 
 def select_function(lat_precalc, tilt_precalc, azim_precalc):
-    b_dict = {'phi': [-1.57, 1.57], 'beta': [0, 1.57], 'gamma': [-3.14, 3.14]}
-
     if lat_precalc is None and tilt_precalc is None and azim_precalc is None:
         func = lambda x, phi, beta, gamma: func_costheta(x, phi, beta, gamma)
-        bounds = ([b_dict['phi'][0], b_dict['beta'][0], b_dict['gamma'][0]],
-                  [b_dict['phi'][1], b_dict['beta'][1], b_dict['gamma'][1]])
         dict_keys = ['latitude_estimate', 'tilt_estimate', 'azimuth_estimate']
 
     if lat_precalc is not None and tilt_precalc is None and azim_precalc is None:
         func = lambda x, beta, gamma: func_costheta(x, np.deg2rad(lat_precalc), beta, gamma)
-        bounds = ([b_dict['beta'][0], b_dict['gamma'][0]],
-                  [b_dict['beta'][1], b_dict['gamma'][1]])
         dict_keys = ['tilt_estimate', 'azimuth_estimate']
 
     if lat_precalc is None and tilt_precalc is not None and azim_precalc is None:
         func = lambda x, phi, gamma: func_costheta(x, phi, np.deg2rad(tilt_precalc), gamma)
-        bounds = ([b_dict['phi'][0], b_dict['gamma'][0]],
-                  [b_dict['phi'][1], b_dict['gamma'][1]])
         dict_keys = ['latitude_estimate', 'azimuth_estimate']
 
     if lat_precalc is None and tilt_precalc is None and azim_precalc is not None:
         func = lambda x, phi, beta: func_costheta(x, phi, beta, np.deg2rad(azim_precalc))
-        bounds = ([b_dict['phi'][0], b_dict['beta'][0]],
-                  [b_dict['phi'][1], b_dict['beta'][1]])
         dict_keys = ['latitude_estimate', 'tilt_estimate']
 
     if lat_precalc is None and tilt_precalc is not None and azim_precalc is not None:
         func = lambda x, phi: func_costheta(x, phi, np.deg2rad(tilt_precalc), np.deg2rad(azim_precalc))
-        bounds = ([b_dict['phi'][0]], [b_dict['phi'][1]])
         dict_keys = ['latitude_estimate']
 
     if lat_precalc is not None and tilt_precalc is None and azim_precalc is not None:
         func = lambda x, beta: func_costheta(x, np.deg2rad(lat_precalc), beta, np.deg2rad(azim_precalc))
-        bounds = ([b_dict['beta'][0]], [b_dict['beta'][1]])
         dict_keys = ['tilt_estimate']
 
     if lat_precalc is not None and tilt_precalc is not None and azim_precalc is None:
-        func = lambda x, gamma: func_costheta(x, np.deg2rad(lat_precalc), np.deg2rad(tilt_precalc),  gamma)
-        bounds = ([b_dict['gamma'][0]], [b_dict['gamma'][1]])
+        func = lambda x, gamma: func_costheta(x, np.deg2rad(lat_precalc), np.deg2rad(tilt_precalc), gamma)
         dict_keys = ['azimuth_estimate']
 
-    init_values = 10*np.ones(len(dict_keys))
+    bounds_dict = {'latitude': [-1.57, 1.57], 'tilt': [0, 1.57], 'azimuth': [-3.14, 3.14]}
+    bounds = []
+    for el in dict_keys:
+        g_param = el.split('_')[0]
+        bounds.append(bounds_dict[g_param])
+
+    bounds = tuple(np.transpose(bounds).tolist())
+    init_values = 10 * np.ones(len(dict_keys))
 
     return func, bounds, init_values, dict_keys
 
