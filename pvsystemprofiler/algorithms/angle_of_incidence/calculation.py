36b2690f8d203c1e40a04bd6d5e9f93240f131fb
alh <a@alhs-MacBook-Air.local>
2020-07-13 09:23:36 -0700

    added docstrings.

--- pvsystemprofiler/algorithms/angle_of_incidence/calculation.py
+++ pvsystemprofiler/algorithms/angle_of_incidence/calculation.py
@@ -1,16 +1,30 @@
 from scipy.optimize import curve_fit
 import numpy as np
 import cvxpy as cvx
-
-
+""" Angle of incidence Module
+This module contains the function for the calculation of the system's latitude, tilt and azimuth when some of them are
+given as precalculates and others are left as unknowns. Unknowns are calculated via fit to the angle of incidence 
+cos(theta) equation (1.6.2) in:
+Duffie, John A., and William A. Beckman. Solar engineering of thermal processes. New York: Wiley, 1991.
+"""
 def run_curve_fit(func, delta, omega, costheta, boolean_daytime_range, init_values, fit_bounds):
+    """
+    :param func: Angle of incidence model function.
+    :param delta: System's declination (array).
+    :param omega: System's hour angle (array).
+    :param costheta: The dependent data. Angle of incidence array used to fit parameters.
+    :param boolean_daytime_range: boolean array specifying days to be used in fitting.
+    :param init_values: Initial guess for the parameters.
+    :param fit_bounds: Lower and upper bounds on parameters.
+    :return: Optimal values for the parameters.
+    """
     costheta_fit = costheta[boolean_daytime_range]
     x = np.array([delta, omega])
+
     popt, pcov = curve_fit(func, x, costheta_fit, p0=np.deg2rad(init_values),
                            bounds=fit_bounds)
     estimates = np.degrees(popt)
     return estimates
-
 
 def find_fit_costheta(data_matrix, clear_index):
     data = np.max(data_matrix, axis=0)
@@ -29,7 +43,18 @@ def find_fit_costheta(data_matrix, clear_index):
     return scale_factor_costheta, costheta_fit
 
 
-def calculate_costheta(func, delta_sys, omega_sys, lat, tilt, azim, est_dict=None, ground_truth=False):
+def calculate_costheta(func, delta_sys, omega_sys, lat=None, tilt=None, azim=None, est_dict=None, ground_truth=False):
+    """
+    :param func: angle of incidence model function.
+    :param delta: System's declination (array).
+    :param omega: System's hour angle (array).
+    :param lat: (optional) System's latitude.
+    :param tilt: (optional) System's tilt.
+    :param azim: (optional)System's azimuth.
+    :param est_dict: directory containig estimated parametes
+    :param ground_truth:
+    :return: angle of incidence array
+    """
     if ground_truth:
         if lat is None or tilt is None or azim is None:
             return None
