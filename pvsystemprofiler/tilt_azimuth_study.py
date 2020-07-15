e7fd4fe9f2c1b32bfce23f4c615defa4d69b6d30
alh <a@alhs-air.lan>
2020-07-15 09:29:27 -0700

    Deleted spaces.

--- pvsystemprofiler/tilt_azimuth_study.py
+++ pvsystemprofiler/tilt_azimuth_study.py
@@ -86,9 +86,7 @@ class TiltAzimuthStudy():
 
         delta_method = np.atleast_1d(delta_method)
         self.find_boolean_daytime()
-        self.omega = find_omega(self.data_sampling, self.num_days, self.lon_precalc, self.day_of_year,
-                                self.gmt_offset)
-
+        self.omega = find_omega(self.data_sampling, self.num_days, self.lon_precalc, self.day_of_year, self.gmt_offset)
         self.scale_factor_costheta, self.costheta_fit = find_fit_costheta(self.data_matrix, self.clear_index)
         self.delta_cooper = delta_cooper(self.day_of_year, self.daily_meas)
         self.delta_spencer = delta_spencer(self.day_of_year, self.daily_meas)
