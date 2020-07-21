939cc14b2bb3a5ab68dd27ec85d4d9430880d620
alh <a@alhs-air.lan>
2020-07-21 10:55:28 -0700

    Corrected bug in conversion to Degrees and docstring typo.

--- pvsystemprofiler/utilities/declination_equation.py
+++ pvsystemprofiler/utilities/declination_equation.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def delta_cooper(day_of_year, daily_meas):
     """"
-    Declination delta1 is estimated from equation (1.6.1a) in:
+    Declination delta is estimated from equation (1.6.1a) in:
     Duffie, John A., and William A. Beckman. Solar engineering of thermal
     processes. New York: Wiley, 1991.
     """
@@ -12,11 +12,11 @@ def delta_cooper(day_of_year, daily_meas):
 
 def delta_spencer(day_of_year, daily_meas):
     """"
-    Declination delta1 is estimated from equation (1.6.1b) in:
+    Declination delta is estimated from equation (1.6.1b) in:
     Duffie, John A., and William A. Beckman. Solar engineering of thermal
     processes. New York: Wiley, 1991.
     """
-    b = np.deg2rad(day_of_year - 1)*360/365
+    b = np.deg2rad((day_of_year - 1)*360/365)
     delta_1 = (180 / np.pi) * (0.006918 - 0.399912 * np.cos(b) + 0.070257 * np.sin(b) - 0.006758 *
                                           np.cos(2 * b) + 0.000907 * np.sin(2 * b) - 0.002697 * np.cos(3 * b) +
                                              0.00148 * np.sin(3 * b))
