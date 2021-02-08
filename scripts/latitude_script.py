2861762f7b827c19259b7f55c39f6f434d28f1f4
Londono <londonoh@pm.me>
2021-02-08 14:33:01 -0800

    Added site_report, updated lon, lat scripts.

--- scripts/latitude_script.py
+++ scripts/latitude_script.py
@@ -4,23 +4,26 @@ import os
 import sys
 from time import time
 
-
-# power data location                                                                                                                 
-data_location = './'
-#output results file location                                                                                                        
-results_file = './data/results_longitude.csv'
-#site list file location                                                                                                              
-site_list_file = './site_list.csv'
-# solar data tools path                                                                                                               
-s1 = '/home/pv-system-profiler/'
-# pv system profiler path                                                                                                            
-s2 = '/home/solar-data-tools/'
-sys.path.append(s1)
-sys.path.append(s2)
+sys.path.append('/home/ubuntu/github/pv-system-profiler/')
+sys.path.append('/home/ubuntu/github/solar-data-tools/')
 
 from solardatatools import DataHandler
 from solardatatools.utilities import progress
+from solardatatools.dataio import load_constellation_data
 from pvsystemprofiler.latitude_study import LatitudeStudy
+import json
+
+try:
+    with open('./param_lat.txt') as f:
+        file_locations = f.read()
+        file_dict = json.loads(file_locations)
+except FileNotFoundError:
+    print('Error reading input file')
+
+results_file = file_dict['results_file']
+site_list_file = file_dict['site_list_file']
+report_file = file_dict['report_file']
+system_descriptory_string = 'ac_power_inv_'
 
 if os.path.isfile(results_file):
     full_df = pd.read_csv(results_file, index_col=0)
@@ -35,10 +38,17 @@ else:
     partial_df_len = None
     checked_systems = []
 
-df_site = pd.read_csv(sites_file, index_col=0)
-df_site['site'] = df_site['site'].apply(str)
-df_site['system'] = df_site['system'].apply(str)
+df_report = pd.read_csv(report_file, index_col=0)
+df_report['site'] = df_report['site'].apply(str)
+df_report['system'] = df_report['system'].apply(str)
 
+df_input = pd.read_csv(site_list_file, index_col=0)
+df_input['site'] = df_input['site'].apply(str)
+df_input['system'] = df_input['system'].apply(str)
+passing_systems = df_report.loc[df_report['passes pipeline'] == True, 'system'].unique().tolist()
+mask = df_input['system'].isin(passing_systems)
+
+df_site = df_input[mask]
 sites = df_site['site'].unique().tolist()
 site_system_dict = {}
 
@@ -48,71 +58,60 @@ for site_ix, site_id in enumerate(sites):
 
 site_run_time = 0
 total_time = 0
-counter = 0
 
 for site_ix, site_id in enumerate(sites[start_at:]):
     t0 = time()
-    msg = 'Site/Accum. run time: {0:2.2f} s/{1:2.2f} m'. \
-        format(site_run_time, total_time / 60.0)
+    msg = 'Site/Accum. run time: {0:2.2f} s/{1:2.2f} m'.format(site_run_time, total_time / 60.0)
 
     progress(site_ix, len(sites), msg, bar_length=20)
     df = load_constellation_data(file_id=site_id)
     dh = DataHandler(df)
-    cols = df.columns
 
-    for site_ix, site_id in enumerate(sites[start_at:]):
+    for sys_ix, sys_id in enumerate(site_system_dict[site_id]):
+        if sys_id not in checked_systems:
+            print(site_id, sys_id)
+            sys_tag = system_descriptory_string + str(sys_id)
+            mask1 = df_site['site'] == site_id
+            mask2 = df_site['system'] == sys_id
+            mask3 = mask1 & mask2
 
-        t0 = time()
-        msg = 'Site/Accum. run time: {0:2.2f} s/{1:2.2f} m'. \
-            format(site_run_time, total_time / 60.0)
+            real_latitude = float(df_site.loc[mask3, 'latitude'])
+            gmt_offset = float(df_site.loc[mask3, 'gmt_offset'])
+            manual_time_shift = int(df_site.loc[df_site['system'] == sys_id, 'time_shift_manual'].values[0])
 
-        progress(site_ix, len(sites), msg, bar_length=20)
-        df = load_constellation_data(file_id=site_id)
-        dh = DataHandler(df)
-        cols = df.columns
+            if manual_time_shift == 1:
+                dh.fix_dst()
 
-        for sys_ix, sys_id in enumerate(site_system_dict[site_id]):
-            if sys_id not in checked_systems:
-                print(site_id, sys_id)
-                counter += 1
-                sys_tag = 'ac_power_inv_' + str(sys_id)
+            try:
+                dh.run_pipeline(power_col=sys_tag, fix_shifts=False, correct_tz=False, verbose=False)
+            except ValueError:
+                max_val = np.nanquantile(df[sys_tag], 0.95)
+                dh.run_pipeline(power_col=sys_tag, fix_shifts=False, correct_tz=True, verbose=False,
+                                max_val=max_val * 3)
 
-                if sys_tag in cols:
-                    data_available = True
-                else:
-                    data_available = False
+            try:
+                passes_estimation = True
+                lat_study = LatitudeStudy(data_handler=dh, lat_true_value=real_latitude)
+                lat_study.run()
+                partial_df = lat_study.results.sort_index().copy()
+            except ValueError:
+                passes_estimation = False
+                partial_df = pd.DataFrame(columns=['declination_method', 'daylight_calculation', \
+                                                   'data_matrix', 'threshold', 'day_selection_method', \
+                                                   'latitude', 'residual'])
 
-                if data_available:
-                    mask1 = df_site['site'] == site_id
-                    mask2 = df_site['system'] == sys_id
-                    mask3 = mask1 & mask2
-
-                    real_latitude = float(df_site.loc[mask3, 'latitude'])
-                    sys_tag = 'ac_power_inv_' + str(sys_id)
-
-                    manual_time_shift = int(df_site.loc[df_site['system'] == sys_id, 'time_shift_manual'].values[0])
-                    dh.run_pipeline(power_col=sys_tag, fix_shifts=False, correct_tz=False, verbose=False)
-
-                    lat_study = LatitudeStudy(data_handler=dh, lat_true_value=real_latitude)
-                    lat_study.run()
-                    partial_df = lat_study.results.copy()
-                    partial_df['site'] = site_id
-                    partial_df['system'] = sys_id
-                    partial_df['length'] = dh.num_days
-                    partial_df['data sampling'] = dh.data_sampling
-                    partial_df['data quality score'] = dh.data_quality_score
-                    partial_df['data clearness score'] = dh.data_clearness_score
-                    partial_df['inverter clipping'] = dh.inverter_clipping
-                    partial_df['time_shift_manual'] = manual_time_shift
-
-                    partial_df = partial_df.reindex(columns=['site', 'system', 'length', 'data sampling',
-                                                             'data quality score', 'data clearness score',
-                                                             'inverter clipping', 'declination_method',
-                                                             'daylight_calculation', 'data_matrix', 'threshold',
-                                                             'day_selection_method', 'latitude', 'residual'])
-                    full_df = full_df.append(partial_df)
-                    full_df.index = np.arange(len(full_df))
-                    full_df.to_csv(results_file)
+            partial_df['site'] = site_id
+            partial_df['system'] = sys_id
+            partial_df['length'] = dh.num_days
+            partial_df['data sampling'] = dh.data_sampling
+            partial_df['data quality score'] = dh.data_quality_score
+            partial_df['data clearness score'] = dh.data_clearness_score
+            partial_df['inverter clipping'] = dh.inverter_clipping
+            partial_df['time shift manual'] = manual_time_shift
+            partial_df['runs estimation'] = passes_estimation
+            full_df = full_df.append(partial_df)
+            full_df.index = np.arange(len(full_df))
+            full_df.to_csv(results_file)
 
     t1 = time()
     site_run_time = t1 - t0
--- scripts/longitude_script.py
+++ scripts/longitude_script.py
@@ -3,23 +3,27 @@ import numpy as np
 import os
 import sys
 from time import time
+import json
 
-# power data location
-data_location = './'
-#output results file location
-results_file = './data/results_longitude.csv'
-#site list file location
-site_list_file = './site_list.csv'
-# solar data tools path
-s1 = '/home/pv-system-profiler/'
-# pv system profiler path
-s2 = '/home/solar-data-tools/'
-sys.path.append(s1)
-sys.path.append(s2)
+sys.path.append('/home/ubuntu/github/pv-system-profiler/')
+sys.path.append('/home/ubuntu/github/solar-data-tools/')
 
 from solardatatools import DataHandler
 from solardatatools.utilities import progress
+from solardatatools.dataio import load_constellation_data
 from pvsystemprofiler.longitude_study import LongitudeStudy
+
+try:
+    with open('./param_lon.txt') as f:
+        file_locations = f.read()
+        file_dict = json.loads(file_locations)
+except FileNotFoundError:
+    print('Error reading input file')
+
+results_file = file_dict['results_file']
+site_list_file = file_dict['site_list_file']
+report_file = file_dict['report_file']
+system_descriptory_string = 'ac_power_inv_'
 
 if os.path.isfile(results_file):
     full_df = pd.read_csv(results_file, index_col=0)
@@ -34,10 +38,17 @@ else:
     partial_df_len = None
     checked_systems = []
 
-df_site = pd.read_csv(site_list_file, index_col=0)
-df_site['site'] = df_site['site'].apply(str)
-df_site['system'] = df_site['system'].apply(str)
+df_report = pd.read_csv(report_file, index_col=0)
+df_report['site'] = df_report['site'].apply(str)
+df_report['system'] = df_report['system'].apply(str)
 
+df_input = pd.read_csv(site_list_file, index_col=0)
+df_input['site'] = df_input['site'].apply(str)
+df_input['system'] = df_input['system'].apply(str)
+passing_systems = df_report.loc[df_report['passes pipeline'] == True, 'system'].unique().tolist()
+mask = df_input['system'].isin(passing_systems)
+
+df_site = df_input[mask]
 sites = df_site['site'].unique().tolist()
 site_system_dict = {}
 
@@ -47,61 +58,61 @@ for site_ix, site_id in enumerate(sites):
 
 site_run_time = 0
 total_time = 0
-counter = 0
 
 for site_ix, site_id in enumerate(sites[start_at:]):
     t0 = time()
-    msg = 'Site/Accum. run time: {0:2.2f} s/{1:2.2f} m'. \
-        format(site_run_time, total_time / 60.0)
+    msg = 'Site/Accum. run time: {0:2.2f} s/{1:2.2f} m'.format(site_run_time, total_time / 60.0)
 
     progress(site_ix, len(sites), msg, bar_length=20)
     df = load_constellation_data(file_id=site_id)
     dh = DataHandler(df)
-    cols = df.columns
 
     for sys_ix, sys_id in enumerate(site_system_dict[site_id]):
         if sys_id not in checked_systems:
             print(site_id, sys_id)
-            counter += 1
-            sys_tag = 'ac_power_inv_' + str(sys_id)
+            sys_tag = system_descriptory_string + str(sys_id)
+            mask1 = df_site['site'] == site_id
+            mask2 = df_site['system'] == sys_id
+            mask3 = mask1 & mask2
 
-            if sys_tag in cols:
-                data_available = True
-            else:
-                data_available = False
+            real_longitude = float(df_site.loc[mask3, 'longitude'])
+            gmt_offset = float(df_site.loc[mask3, 'gmt_offset'])
+            manual_time_shift = int(df_site.loc[df_site['system'] == sys_id, 'time_shift_manual'].values[0])
 
-            if data_available: 
+            if manual_time_shift == 1:
+                dh.fix_dst()
 
-                mask1 = df_site['site'] == site_id
-                mask2 = df_site['system'] == sys_id
-                mask3 = mask1 & mask2
-
-                real_longitude = float(df_site.loc[mask3, 'longitude'])
-                gmt_offset = float(df_site.loc[mask3, 'gmt_offset'])
-                counter += 1
-                sys_tag = 'ac_power_inv_' + str(sys_id)
-                manual_time_shift = int(df_site.loc[df_site['system'] == sys_id, 'time_shift_manual'].values[0])
-
-
+            try:
                 dh.run_pipeline(power_col=sys_tag, fix_shifts=False, correct_tz=False, verbose=False)
-
+            except ValueError:
+                max_val = np.nanquantile(df[sys_tag], 0.95)
+                dh.run_pipeline(power_col=sys_tag, fix_shifts=False, correct_tz=True, verbose=False,
+                                max_val=max_val * 3)
+            try:
+                passes_estimation = True
                 lon_study = LongitudeStudy(data_handler=dh, gmt_offset=gmt_offset, true_value=real_longitude)
-
                 lon_study.run(verbose=False)
-
                 partial_df = lon_study.results.sort_index().copy()
+            except ValueError:
+                passes_estimation = False
+                partial_df = pd.DataFrame(columns=['longitude', 'estimator', 'eot_calculation', 'solar_noon_method',
+                                                   'day_selection_method', 'data_matrix', 'residual', 'site', 'system',
+                                                   'length', 'data sampling', 'data quality score',
+                                                   'data clearness score', 'inverter clipping', 'time shift manual'])
+                partial_df.loc[0, :] = np.nan
 
-                partial_df['site'] = site_id
-                partial_df['system'] = sys_id
-                partial_df['length'] = dh.num_days
-                partial_df['data sampling'] = dh.data_sampling
-                partial_df['data quality score'] = dh.data_quality_score
-                partial_df['data clearness score'] = dh.data_clearness_score
-                partial_df['inverter clipping'] = dh.inverter_clipping
-                partial_df['time_shift_manual'] = manual_time_shift
-                full_df = full_df.append(partial_df)
-                full_df.index = np.arange(len(full_df))
-                full_df.to_csv(results_file)
+            partial_df['site'] = site_id
+            partial_df['system'] = sys_id
+            partial_df['length'] = dh.num_days
+            partial_df['data sampling'] = dh.data_sampling
+            partial_df['data quality score'] = dh.data_quality_score
+            partial_df['data clearness score'] = dh.data_clearness_score
+            partial_df['inverter clipping'] = dh.inverter_clipping
+            partial_df['time shift manual'] = manual_time_shift
+            partial_df['runs estimation'] = passes_estimation
+            full_df = full_df.append(partial_df)
+            full_df.index = np.arange(len(full_df))
+            full_df.to_csv(results_file)
 
     t1 = time()
     site_run_time = t1 - t0
