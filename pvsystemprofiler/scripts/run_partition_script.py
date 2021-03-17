f8469717096c2b7b5d2e70f516af0205752f6f6e
Londono <londonoh@pm.me>
2021-03-17 14:31:07 -0700

    Updated the result retrieving and consolidation algorithm.

--- pvsystemprofiler/scripts/run_partition_script.py
+++ pvsystemprofiler/scripts/run_partition_script.py
@@ -12,6 +12,7 @@ from modules.script_functions import enumerate_files
 from modules.script_functions import get_s3_bucket_and_prefix
 from modules.script_functions import copy_to_s3
 
+
 def get_remote_output_files(partitions, username, destination_dict):
     os.system('mkdir' + ' ' + destination_dict)
     for part_id in partitions:
@@ -134,34 +135,28 @@ if __name__ == '__main__':
     check_json = str(sys.argv[21])
 
     '''
-    :create_input_file: True if a csv file with the system's information to be generated. False if provided.
-    :input file location: a csv file with the system's information.
+    :create_input_file: True if a csv file with the system's information to be generated. False if file provided.
+    :input file location: The csv file with the system's information. If create_input_file=True a file with the 
+    specified name and location is created.
     :ssh_key_file: .pem aws key file.
     :aws_username: aws linux username in instances.
     :aws_instance_name: aws name key used to identify instances to be used in the partitioning.
     :aws_region: region as specified by aws. For example: 'us-west-1'.
     :aws_client: in most cases "ec2".
     :script_name: name of the script that will be run partitioned.
-    :script_location: directory where script_name is located. 
-    :output_folder location: Folder where local results will be saved. This folder will be created during script execution.
-    :data_source: source of the data to be analyzed.
+    :script_location: full path to directory where script_name is located. 
+    :output_folder location: Full path to folder where local results will be saved. This folder will be created during 
+    script execution.
     :power_column_id: id given to the power column to be analyzed.
     :global_output_directory:  directory where consolidated results are saved.
     :global_output_file:  name of csv file with the consolidated results.
     :time_shift_inspection: indicate if manual time shift inspection should be taken into account for pipeline run.
-    :s3_location: Absolute path to s3 location of files.
+    :s3_location: Absolute path to s3 location of files with power data.
     :file_label:  Repeating portion of data files label. If 'None', no file label is used. 
     :fix_time_shifts: String, 'True' or 'False', determines if time shifts are fixed when running the pipeline
     :time_zone_correction: String, 'True' or 'False', determines if time zone correction is performed when running  the
-     pipeline
+    pipeline
     :check_json: String, 'True' or 'False'. Check json file for location information. 
-    
-    
-    
-    file_label = str(sys.argv[18])
-    fix_time_shifts = str(sys.argv[19])
-    time_zone_correction = str(sys.argv[20])
-    check_json = str(sys.argv[21])
     '''
 
     if create_input_file == 'True':
@@ -174,7 +169,6 @@ if __name__ == '__main__':
         bucket, prefix = get_s3_bucket_and_prefix(input_file_location)
         copy_to_s3('./generated_site_list.csv', bucket, prefix)
 
-
     main_class = get_config(ifl=input_file_location, ofl=output_folder_location, skf=ssh_key_file, au=aws_username,
                             ain=aws_instance_name, ar=aws_region, ac=aws_client, ds=data_source, pcid=power_column_id,
                             gof=global_output_file, god=global_output_directory, tsi=time_shift_inspection,
@@ -185,8 +179,6 @@ if __name__ == '__main__':
     df = pd.read_csv(input_file_location, index_col=0)
 
     main(df, ec2_instances, input_file_location, output_folder_location, ssh_key_file, aws_username,
-                             aws_instance_name, aws_region, aws_client, script_name, script_location, data_source,
-                             power_column_id, time_shift_inspection, s3_location, n_files, file_label, fix_time_shifts,
-                             time_zone_correction, check_json)
-
-
+         aws_instance_name, aws_region, aws_client, script_name, script_location, data_source,
+         power_column_id, time_shift_inspection, s3_location, n_files, file_label, fix_time_shifts,
+         time_zone_correction, check_json)
