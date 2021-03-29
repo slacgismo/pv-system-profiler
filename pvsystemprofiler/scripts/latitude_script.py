cc121d264e34cf2b14476e556d0a2854ac5266c3
Londono <londonoh@pm.me>
2021-03-29 08:46:03 -0700

    Deleted old versions of scripts renamed new ones.

--- pvsystemprofiler/scripts/latitude_script.py
+++ pvsystemprofiler/scripts/latitude_script.py
@@ -1,122 +1,203 @@
+import sys
+import os
 import pandas as pd
 import numpy as np
-import os
-import sys
 from time import time
-
 sys.path.append('/home/ubuntu/github/pv-system-profiler/')
-sys.path.append('/home/ubuntu/github/solar-data-tools/')
-
+#sys.path.append('/home/ubuntu/github/solar-data-tools/')
+#sys.path.append('/Users/londonoh/Documents/github/pv-system-profiler/')
+#sys.path.append('/Users/londonoh/Documents/github/solar-data-tools/')
 from solardatatools import DataHandler
 from solardatatools.utilities import progress
-from solardatatools.dataio import load_constellation_data
+from modules.script_functions import run_failsafe_pipeline
+from modules.script_functions import resume_run
+from modules.script_functions import load_generic_data
+from modules.script_functions import enumerate_files
+from modules.script_functions import get_checked_sites
+from modules.script_functions import get_s3_bucket_and_prefix
+from modules.script_functions import create_json_dict
+from modules.script_functions import string_to_boolean
+from modules.script_functions import log_file_versions
 from pvsystemprofiler.latitude_study import LatitudeStudy
-import json
+from modules.script_functions import filename_to_siteid
 
-try:
-    with open('./param_lat.txt') as f:
-        file_locations = f.read()
-        file_dict = json.loads(file_locations)
-except FileNotFoundError:
-    print('Error reading input file')
+def run_failsafe_lat_estimation(dh_in, real_latitude):
 
-results_file = file_dict['results_file']
-site_list_file = file_dict['site_list_file']
-report_file = file_dict['report_file']
-system_descriptory_string = 'ac_power_inv_'
+    try:
+        runs_lat_estimation = True
+        lat_study = LatitudeStudy(data_handler=dh_in, lat_true_value=real_latitude)
+        lat_study.run()
+        lat_study.run()
+        p_df = lat_study.results.sort_index().copy()
+    except:
+        runs_lat_estimation = False
+        p_df = pd.DataFrame(columns=['declination_method', 'daylight_calculation',  'data_matrix', 'threshold',
+                                     'day_selection_method', 'latitude', 'residual'])
+        p_df.loc[0, :] = np.nan
+    return p_df, runs_lat_estimation
 
-if os.path.isfile(results_file):
-    full_df = pd.read_csv(results_file, index_col=0)
-    start_at = len(full_df['site'].unique()) - 1
-    partial_df_len = int(len(full_df) / len(full_df['system'].unique()))
-    full_df['site'] = full_df['site'].apply(str)
-    full_df['system'] = full_df['system'].apply(str)
-    checked_systems = full_df['system'].unique().tolist()
-else:
-    start_at = 0
-    full_df = pd.DataFrame()
-    partial_df_len = None
-    checked_systems = []
+def evaluate_systems(df, df_ground_data, power_column_label, site_id, time_shift_inspection,
+                     fix_time_shifts, time_zone_correction, json_file_dict=None):
+    ll = len(power_column_label)
+    cols = df.columns
+    i = 0
+    partial_df = pd.DataFrame()
+    for col_label in cols:
+        if col_label.find(power_column_label) != -1:
+            system_id = col_label[ll:]
+            #print(site_id, system_id)
+            if system_id in df_ground_data['system'].tolist():
+                #print(site_id, system_id)
+                i += 1
+                sys_tag = power_column_label + system_id
 
-df_report = pd.read_csv(report_file, index_col=0)
-df_report['site'] = df_report['site'].apply(str)
-df_report['system'] = df_report['system'].apply(str)
+                real_latitude = float(df_ground_data.loc[df_ground_data['system'] == system_id, 'latitude'])
 
-df_input = pd.read_csv(site_list_file, index_col=0)
-df_input['site'] = df_input['site'].apply(str)
-df_input['system'] = df_input['system'].apply(str)
-passing_systems = df_report.loc[df_report['passes pipeline'] == True, 'system'].unique().tolist()
-mask = df_input['system'].isin(passing_systems)
+                dh = DataHandler(df)
+                if time_shift_inspection:
+                    manual_time_shift = int(df_ground_data.loc[df_ground_data['system'] == system_id,
+                                                                'time_shift_manual'].values[0])
+                    if manual_time_shift == 1:
+                        dh.fix_dst()
+                try:
+                    run_failsafe_pipeline(dh, df, sys_tag, fix_time_shifts, time_zone_correction)
+                    passes_pipeline = True
+                except:
+                    passes_pipeline = False
+                if passes_pipeline:
+                    results_df, passes_estimation = run_failsafe_lat_estimation(dh, real_latitude)
+                    results_df['length'] = dh.num_days
+                    results_df['data sampling'] = dh.data_sampling
+                    results_df['data quality score'] = dh.data_quality_score
+                    results_df['data clearness score'] = dh.data_clearness_score
+                    results_df['inverter clipping'] = dh.inverter_clipping
+                    results_df['runs estimation'] = passes_estimation
+                else:
+                    results_df = pd.DataFrame()
+                    results_df['length'] = np.nan
+                    results_df['data sampling'] = np.nan
+                    results_df['data quality score'] = np.nan
+                    results_df['data clearness score'] = np.nan
+                    results_df['inverter clipping'] = np.nan
+                    results_df['runs estimation'] = np.nan
 
-df_site = df_input[mask]
-sites = df_site['site'].unique().tolist()
-site_system_dict = {}
+                results_df['site'] = site_id
+                results_df['system'] = system_id
 
-for site_ix, site_id in enumerate(sites):
-    systems_in_site = df_site[df_site['site'] == site_id]['system'].values.tolist()
-    site_system_dict[site_id] = systems_in_site
+                if time_shift_inspection:
+                    results_df['manual_time shift'] = manual_time_shift
 
-site_run_time = 0
-total_time = 0
+                partial_df = partial_df.append(results_df)
+    return partial_df
 
-for site_ix, site_id in enumerate(sites[start_at:]):
-    t0 = time()
-    msg = 'Site/Accum. run time: {0:2.2f} s/{1:2.2f} m'.format(site_run_time, total_time / 60.0)
 
-    progress(site_ix, len(sites), msg, bar_length=20)
-    df = load_constellation_data(file_id=site_id)
-    dh = DataHandler(df)
+def main(input_file, df_ground_data, n_files, s3_location, file_label, power_column_label, full_df, output_file,
+         time_shift_inspection, fix_time_shifts, time_zone_correction, check_json, ext='.csv'):
+    site_run_time = 0
+    total_time = 0
+    s3_bucket, prefix = get_s3_bucket_and_prefix(s3_location)
 
-    for sys_ix, sys_id in enumerate(site_system_dict[site_id]):
-        if sys_id not in checked_systems:
-            print(site_id, sys_id)
-            sys_tag = system_descriptory_string + str(sys_id)
-            mask1 = df_site['site'] == site_id
-            mask2 = df_site['system'] == sys_id
-            mask3 = mask1 & mask2
+    full_site_list = enumerate_files(s3_bucket, prefix)
+    full_site_list = filename_to_siteid(full_site_list)
 
-            real_latitude = float(df_site.loc[mask3, 'latitude'])
-            gmt_offset = float(df_site.loc[mask3, 'gmt_offset'])
-            manual_time_shift = int(df_site.loc[df_site['system'] == sys_id, 'time_shift_manual'].values[0])
+    previously_checked_site_list = get_checked_sites(full_df)
+    file_list = list(set(full_site_list) - set(previously_checked_site_list))
 
-            if manual_time_shift == 1:
-                dh.fix_dst()
+    if check_json:
+        json_files = enumerate_files(s3_bucket, prefix, extension='.json')
+        print('Generating system list from json files')
+        json_file_dict = create_json_dict(json_files, s3_location)
+        print('List generation completed')
+    else:
+        json_file_dict = None
 
-            try:
-                dh.run_pipeline(power_col=sys_tag, fix_shifts=False, correct_tz=False, verbose=False)
-            except ValueError:
-                max_val = np.nanquantile(df[sys_tag], 0.95)
-                dh.run_pipeline(power_col=sys_tag, fix_shifts=False, correct_tz=True, verbose=False,
-                                max_val=max_val * 3)
+    if input_file != 'None':
+        input_file_df = pd.read_csv(input_file, index_col=0)
+        site_list = input_file_df['site'].apply(str)
+        site_list = site_list.tolist()
+        #input_file_list = siteid_to_filename(site_list, file_label, ext)
+        #file_list = list(set(input_file_list) & set(file_list))
+        manually_checked_sites = df_ground_data['site_file'].apply(str).tolist()
+        file_list = list(set(site_list) & set(file_list) & set(manually_checked_sites))
 
-            try:
-                passes_estimation = True
-                lat_study = LatitudeStudy(data_handler=dh, lat_true_value=real_latitude)
-                lat_study.run()
-                partial_df = lat_study.results.sort_index().copy()
-            except ValueError:
-                passes_estimation = False
-                partial_df = pd.DataFrame(columns=['declination_method', 'daylight_calculation', \
-                                                   'data_matrix', 'threshold', 'day_selection_method', \
-                                                   'latitude', 'residual'])
+    file_list.sort()
 
-            partial_df['site'] = site_id
-            partial_df['system'] = sys_id
-            partial_df['length'] = dh.num_days
-            partial_df['data sampling'] = dh.data_sampling
-            partial_df['data quality score'] = dh.data_quality_score
-            partial_df['data clearness score'] = dh.data_clearness_score
-            partial_df['inverter clipping'] = dh.inverter_clipping
-            partial_df['time shift manual'] = manual_time_shift
-            partial_df['runs estimation'] = passes_estimation
+    if n_files != 'all':
+        file_list = file_list[:int(n_files)]
+    for file_ix, file_id in enumerate(file_list):
+        t0 = time()
+        msg = 'Site/Accum. run time: {0:2.2f} s/{1:2.2f} m'.format(site_run_time, total_time / 60.0)
+        progress(file_ix, len(file_list), msg, bar_length=20)
+        if file_label != '':
+            i = file_id.find(file_label)
+            site_id = file_id[:i]
+        else:
+            site_id = file_id.split('.')[0]
+
+        df = load_generic_data(s3_location, file_label, site_id)
+        partial_df = evaluate_systems(df, df_ground_data, power_column_label, site_id, time_shift_inspection,
+                                      fix_time_shifts, time_zone_correction, json_file_dict)
+        if not partial_df.empty:
             full_df = full_df.append(partial_df)
             full_df.index = np.arange(len(full_df))
-            full_df.to_csv(results_file)
+            full_df.to_csv(output_file)
+            t1 = time()
+            site_run_time = t1 - t0
+            total_time += site_run_time
 
-    t1 = time()
-    site_run_time = t1 - t0
-    total_time += site_run_time
+    msg = 'Site/Accum. run time: {0:2.2f} s/{1:2.2f} m'.format(site_run_time, total_time / 60.0)
+    if len(file_list) != 0:
+        progress(len(file_list), len(file_list), msg, bar_length=20)
+    print('finished')
+    return
 
-msg = 'Site/Accum. run time: {0:2.2f} s/{1:2.2f} m'. \
-    format(site_run_time, total_time / 60.0)
-progress(len(sites), len(sites), msg, bar_length=20)
+
+if __name__ == '__main__':
+    '''
+        :input_file:  csv file containing list of sites to be evaluated. 'None' if no input file is provided.
+        :df_ground_data: location of pandas dataframe containing longitude and gmt offset for each system.
+        :n_files: number of files to read. If 'all' all files in folder are read.
+        :s3_location: Absolute path to s3 location of files.
+        :file_label:  Repeating portion of data files label. If 'None', no file label is used. 
+        :power_column_label: Repeating portion of the power column label. 
+        :output_file: Absolute path to csv file containing report results.
+        :time_shift_inspection: String, 'True' or 'False'. Determines indicates if manual time shift inspection should 
+        be taken into account for pipeline run.
+        :fix_time_shifts: String, 'True' or 'False', determines if time shifts are fixed when running the pipeline
+        :time_zone_correction: String, 'True' or 'False', determines if time zone correction is performed when running 
+        the pipeline
+        :check_json: String, 'True' or 'False'. Check json file for location information. 
+        '''
+
+    input_file = str(sys.argv[1])
+    n_files = str(sys.argv[2])
+    s3_location = str(sys.argv[3])
+    file_label = str(sys.argv[4])
+    power_column_label = str(sys.argv[5])
+    output_file = str(sys.argv[6])
+    time_shift_inspection = str(sys.argv[7])
+    fix_time_shifts = string_to_boolean(str(sys.argv[8]))
+    time_zone_correction = string_to_boolean(str(sys.argv[9]))
+    check_json = string_to_boolean(str(sys.argv[10]))
+    df_ground_data = str(sys.argv[11])
+
+    local_output_folder = output_file.split('data')[0]
+    #log_file_versions('solar_data_tools', local_output_folder)
+
+    if file_label == 'None':
+        file_label = ''
+
+    full_df, checked_systems, start_at = resume_run(output_file)
+    df_ground_data = pd.read_csv('s3://pv.insight.misc/report_files/constellation_site_list.csv', index_col=0)
+    df_ground_data = df_ground_data[~df_ground_data['time_shift_manual'].isnull()]
+    df_ground_data['time_shift_manual'] = df_ground_data['time_shift_manual'].apply(int)
+    df_ground_data = df_ground_data[df_ground_data['time_shift_manual'].isin([0, 1])]
+    df_ground_data['site'] = df_ground_data['site'].apply(str)
+    df_ground_data['system'] = df_ground_data['system'].apply(str)
+    df_ground_data['site_file'] = df_ground_data['site'].apply(lambda x: str(x) + '_20201006_composite')
+
+
+
+
+    main(input_file, df_ground_data, n_files, s3_location, file_label, power_column_label, full_df, output_file,
+        time_shift_inspection, fix_time_shifts, time_zone_correction, check_json)
