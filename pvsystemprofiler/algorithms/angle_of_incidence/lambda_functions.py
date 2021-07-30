69d9117a0a9153c4b084f86de76021b7fa01cedc
Londono <londonoh@pm.me>
2021-07-30 07:43:25 -0700

    updated docstring

--- pvsystemprofiler/algorithms/angle_of_incidence/lambda_functions.py
+++ pvsystemprofiler/algorithms/angle_of_incidence/lambda_functions.py
@@ -1,11 +1,11 @@
 """
 This module is used to set the hour_angle_equation in terms of the unknowns. The hour equation is a function of the
-declination (delta), the hour angle (omega) , latitudeitude (phi), tilt (beta) and azimuth (gamma). The declination and the
-hour angle are treated as input parameters for all cases. latitudeitude, tilt and azimuth can be given as input parameters
-(precalculatitudees) or left as unknowns. In total, seven different combinations arise from having these three parameters
+declination (delta), the hour angle (omega) , latitude (phi), tilt (beta) and azimuth (gamma). The declination and the
+hour angle are treated as input parameters for all cases. Latitude, tilt and azimuth can be given as input parameters
+ or left as unknowns (`None`). In total, seven different combinations arise from having these three parameters
 as an inputs or as a unknowns. The seven conditionals below correspond to those combinations. The output function "func"
 is used as one of the inputs to run_curve_fit which in turn is used to fit the unknowns. The function other outputs is
-the 'bounds' tuple containing the bounds for the variables. Bounds for latitudeitude are -90 to 90. Bounds for tilt are 0 to
+the 'bounds' tuple containing the bounds for the variables. Bounds for latitude are -90 to 90. Bounds for tilt are 0 to
 90. Bounds for azimuth  are -180 to 180. It is noted that, theoretically, bounds for tilt are 0 to 180 (Duffie, John A.,
  and William A. Beckman. Solar engineering of thermal processes. New York: Wiley, 1991.). However a value of tilt >90
  would mean that that the surface has a downward-facing component, which is not the case of the current application.
@@ -16,7 +16,7 @@ import numpy as np
 
 def select_function(latitude=None, tilt=None, azimuth=None):
     """
-    :param latitude: (optional) latitude input valie in Degrees.
+    :param latitude: (optional) latitude input value in Degrees.
     :param tilt: (optional) Tilt input value in Degrees.
     :param azimuth: (optional) Azimuth input value in Degrees.
     :return: Customized function 'func' and 'bounds' tuple.
