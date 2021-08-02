dd1cda68c7ac0606c5cfd96e04a0b53027c73b05
Londono <londonoh@pm.me>
2021-08-02 12:31:50 -0700

    corrected error pasing eot

--- pvsystemprofiler/algorithms/longitude/calculation.py
+++ pvsystemprofiler/algorithms/longitude/calculation.py
@@ -4,5 +4,6 @@ from pvsystemprofiler.algorithms.longitude.direct_calculation import calc_lon
 
 def calculate_longitude(eot, solarnoon, days, gmt_offset):
     sn = 60 * solarnoon[days]  # convert hours to minutes
-    estimates = calc_lon(sn, eot, gmt_offset)
+    eot_days = eot[days]
+    estimates = calc_lon(sn, eot_days, gmt_offset)
     return np.nanmedian(estimates)
