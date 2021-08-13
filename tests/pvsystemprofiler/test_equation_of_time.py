7d71e163ff89e722433c9d4d141f1650fece8f42
Derin Serbetcioglu <serbetd@gmail.com>
2021-08-13 11:49:07 -0700

    eot tests pass, fit longitude test fails

---
+++ tests/pvsystemprofiler/test_equation_of_time.py
@@ -0,0 +1,39 @@
+import unittest
+import os
+from pathlib import Path
+import numpy as np
+path = Path.cwd().parent.parent
+os.chdir(path)
+from pvsystemprofiler.utilities.equation_of_time import eot_da_rosa, eot_duffie
+
+
+class TestEquationOfTime(unittest.TestCase):
+
+    # importing input for both eot tests
+    data_file_path = Path(__file__).parent.parent.joinpath("fixtures/longitude_fitting/eot_input.csv")
+    with open(data_file_path) as file:
+            input_data = np.genfromtxt(file, delimiter=',')
+
+
+    def test_eot_duffie(self):
+
+        expected_data_file_path = Path(__file__).parent.parent.joinpath("fixtures/longitude_fitting/eot_duffie_output.csv")
+        with open(expected_data_file_path) as file:
+            expected_output = np.genfromtxt(file, delimiter=',')
+
+        actual_output = eot_duffie(self.input_data)
+        np.testing.assert_array_almost_equal(actual_output, expected_output)
+
+
+    def test_eot_da_rosa(self):
+
+        expected_data_file_path = Path(__file__).parent.parent.joinpath("fixtures/longitude_fitting/eot_da_rosa_output.csv")
+        with open(expected_data_file_path) as file:
+            expected_output = np.genfromtxt(file, delimiter=',')
+
+        actual_output = eot_da_rosa(self.input_data)
+        np.testing.assert_array_almost_equal(actual_output, expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
