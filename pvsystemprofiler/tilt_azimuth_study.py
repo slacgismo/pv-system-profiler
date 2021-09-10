fd3bb550d128de4a4e203936c5bd58c97fc350f6
alejandro <londonoh@pm.me>
2021-09-10 12:40:57 -0700

    added atleast_1d to threshold and input values

--- pvsystemprofiler/tilt_azimuth_study.py
+++ pvsystemprofiler/tilt_azimuth_study.py
@@ -85,8 +85,12 @@ class TiltAzimuthStudy():
         self.daytime_threshold_fit = None
         if cvx_parameter is None:
             self.threshold_x1 = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+        else:
+            self.threshold_x1 = np.atleast_1d(cvx_parameter)
         if threshold_quantile is None:
             self.threshold_x2 = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+        else:
+            self.threshold_x2 = np.atleast_1d(threshold_quantile)
         # data specific variables
         self.day_of_year = self.data_handler.day_index.dayofyear
         self.num_days = self.data_handler.num_days
